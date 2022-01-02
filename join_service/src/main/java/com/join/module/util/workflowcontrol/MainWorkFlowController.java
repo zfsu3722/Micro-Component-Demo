a6bbbde56e9306f2685a64e6cc372285c951e7e9
Zxg <zfsu3722@hotmail.com>
2022-01-02 22:51:04 +0800

    Update MainWorkFlowController.java

--- join_service/src/main/java/com/join/module/util/workflowcontrol/MainWorkFlowController.java
+++ join_service/src/main/java/com/join/module/util/workflowcontrol/MainWorkFlowController.java
@@ -1,21 +1,24 @@
 package com.join.module.util.workflowcontrol;
 
+
 import com.join.entities.Context;
 import com.join.entities.NodeConfig;
 import com.join.entities.OutputParam;
-import com.join.module.util.common.*;
+import com.join.module.util.common.ParseContextForFunc;
+import com.join.module.util.common.ParseInParam;
+import com.join.module.util.common.ParseOutSend;
+import com.join.module.util.common.SpringContextUtil;
 import com.join.module.util.parse_xml.NodeFuncXmlLoad;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- *
- */
 public class MainWorkFlowController {
+	public final static String CONTROL_FLAG = "CONTROL";
 
 	public static Map<String,Object> JobExececute(String key,String requestId,HashMap<String, Object> control_param){
+
 		NodeConfig nodeConfig;
 		nodeConfig = NodeFuncXmlLoad.getNodeConfig(key);
 		nodeConfig.setRequestId(requestId);
@@ -36,8 +39,11 @@ public class MainWorkFlowController {
 		Context context;
 		List<OutputParam> outParams;
 		HashMap<String, Object> returnMap = null;
+		HashMap<String, Object> sendReturnMap = null;
 		Map<String,Object> outPutMap = null;
+		HashMap<String,Object> outMap;
 		String preKey;
+		String destRef;
 		String send_flag;
 
 		outParams = nodeConfig.getOutParam();
@@ -47,23 +53,37 @@ public class MainWorkFlowController {
 		jobBeanExecutor = (IJobBeanExecutor) SpringContextUtil.getBean(execBeanName);
 		try {
 			outPutMap = jobBeanExecutor.ExecuteJobBean(inParamsMap);
-			send_flag = (String) outPutMap.get(PublicConstant.SEND_FLAG);
-			if (send_flag.equals(PublicConstant.YES_SEND_FLAG)){
-				outPutMap.remove(PublicConstant.SEND_FLAG);
-				for (OutputParam o : outParams){
+
+			for (OutputParam o : outParams){
+				destRef = o.getDestRef();
+				if(o.getDestRef().equals(CONTROL_FLAG)){
 					returnMap = ParseOutSend.parseDestRef(o,preKey, (HashMap<String, Object>) outPutMap);
-					if (!isOk(returnMap)){
-						System.out.println("Data transfer failed:"+o.toString());
-						break;
-					}
+				}else {
+					outMap = ParseOutSend.parseDestRef(o,preKey, (HashMap<String, Object>) outPutMap);
+					sendReturnMap = ParseOutSend.sendToDest(destRef,outMap);
 				}
-			}else if (send_flag.equals(PublicConstant.NO_SEND_FLAG)){
-				return outPutMap;
+				if (!o.getDestRef().equals(CONTROL_FLAG)&&!isOk(sendReturnMap)){
+					System.out.println("Data transfer failed:"+o.toString());
+					break;
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+		if(sendReturnMap!=null)
+		{
+			if(returnMap==null) {
+				returnMap = new HashMap<String, Object>();
+			}
+			if(!isOk(sendReturnMap)) {
+				returnMap.put("flag", 0);
+			}
+			else
+			{
+				returnMap.put("flag", 1);
+			}
+		}
 		return returnMap;
 	}
 	private static boolean isOk(HashMap<String, Object> returnMap){
@@ -76,5 +96,6 @@ public class MainWorkFlowController {
 		}else if (i == 0){}
 		return ret;
 	}
+
 
 }
