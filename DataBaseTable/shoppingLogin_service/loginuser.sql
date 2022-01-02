4345454cc25a7db0ddfe7494177918669cd69072
zfsu3722 <55656680+zfsu3722@users.noreply.github.com>
2022-01-02 15:32:48 +0800

    Add files via upload

---
+++ DataBaseTable/shoppingLogin_service/loginuser.sql
@@ -0,0 +1,14 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.40 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `loginuser` (
+	`loginName` varchar (765),
+	`userPassword` varchar (765)
+); 
+insert into `loginuser` (`loginName`, `userPassword`) values('Emma Stone','123');
+insert into `loginuser` (`loginName`, `userPassword`) values('','');
+insert into `loginuser` (`loginName`, `userPassword`) values('John Legend','123');
